15bbcf5699854b103c428b7c86eff85f1f922ce5
Wesley Holt <holtwes1587@gmail.com>
2021-02-09 00:25:19 -0500

    Resubmitting Daniel's commits

---
+++ Group3_project/Assets/MovementOfCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementOfCharacter : MonoBehaviour
+{
+float Speed = 5;
+ 
+void Update()
+{
+    
+ 
+    if (Input.GetKey("right"))
+    {
+        transform.position += transform.right * Speed * Time.deltaTime;
+    }
+    else if (Input.GetKey("left"))
+    {
+        transform.position -= transform.right * Speed * Time.deltaTime;
+    }
+}
+}
