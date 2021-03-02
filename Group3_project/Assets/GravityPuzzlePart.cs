d37bf7377cb4fbd406403d83a63c023eb3c11e48
Wesley Holt <holtwes1587@gmail.com>
2021-03-02 14:14:18 -0500

    did some work on a script for the gravity spell

---
+++ Group3_project/Assets/GravityPuzzlePart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPuzzlePart : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //update gravity if hit by gravity spell
+     void OnCollisionEnter(Collision other) {
+        if(other.gameObject.name == "Player");
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().velocity = new Vector2 (GetComponent<Rigidbody> ().velocity.x, -Physics.gravity.y);    
+        }
+    }
+}
