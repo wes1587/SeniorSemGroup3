8f2b6ec89d510d1b4b943b4be45da58c405dcf7e
Wesley Holt <holtwes1587@gmail.com>
2021-03-04 20:00:16 -0500

    fixed the movement script, gravityPuzzlePart needs some work

--- Group3_project/Assets/GravityPuzzlePart.cs
+++ Group3_project/Assets/GravityPuzzlePart.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GravityPuzzlePart : MonoBehaviour
 {
-     public float spellDuration;
-    private float spellTempTime = 0f;
+    public float spellDuration;
+    float spellTempTime = 0f;
 
 
     // Start is called before the first frame update
@@ -17,20 +17,19 @@ public class GravityPuzzlePart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(spellTempTime == 0){
-            GetComponent<Rigidbody>().useGravity = true;
-        }
-        spellTempTime -= Time.deltaTime;
+        // if(spellTempTime == 0){
+        //     GetComponent<Rigidbody>().useGravity = true;
+        // }
+        // spellTempTime -= Time.deltaTime;
     }
 
-    //update gravity if hit by gravity spell
-     void OnCollisionEnter(Collision other) {
-        if(other.gameObject.name == "Player");
-        {
-            GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<Rigidbody>().velocity = new Vector2 (GetComponent<Rigidbody> ().velocity.x, -Physics.gravity.y);
-             spellTempTime = spellDuration;   
-            GetComponent<Rigidbody>().useGravity = true;
-        }
-    }
+    // //update gravity if hit by gravity spell
+    //  void OnCollisionEnter(Collision object) {
+    //     if(object.gameObject.name == "Player");
+    //     {
+    //         GetComponent<Rigidbody>().useGravity = false;
+    //         GetComponent<Rigidbody>().velocity = new Vector2 (GetComponent<Rigidbody>().velocity.x, -Physics.gravity.y);
+    //         spellTempTime = spellDuration;   
+    //     }
+    // }
 }
