c00660b1a33fffe582344b2533f7ab1b642058a3
Wesley Holt <holtwes1587@gmail.com>
2021-03-07 14:40:34 -0500

    adjusted the gravity spell to work better

--- Group3_project/Assets/GravityPuzzlePart.cs
+++ Group3_project/Assets/GravityPuzzlePart.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class GravityPuzzlePart : MonoBehaviour
 {
-    public float spellDuration;
-    float spellTempTime = 0f;
+    public bool isActive = false;
+    // public float spellDuration;
+    // float spellTempTime = 0f;
 
 
     // Start is called before the first frame update
@@ -17,13 +18,21 @@ public class GravityPuzzlePart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(spellTempTime == 0){
-        //     GetComponent<Rigidbody>().useGravity = true;
-        // }
-        // spellTempTime -= Time.deltaTime;
+        if(!isActive){
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().velocity = new Vector2 (GetComponent<Rigidbody>().velocity.x, Physics.gravity.y);
+        }
+        else{
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().velocity = new Vector2 (GetComponent<Rigidbody>().velocity.x, -Physics.gravity.y);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Q)) {
+            isActive = !isActive;
+        }
     }
 
-    // //update gravity if hit by gravity spell
+    ////update gravity if hit by gravity "spell"
     //  void OnCollisionEnter(Collision object) {
     //     if(object.gameObject.name == "Player");
     //     {
