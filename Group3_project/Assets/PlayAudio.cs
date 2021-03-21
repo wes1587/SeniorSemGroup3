e162449ce5941d2ed097da12670517b3b0c38aad
Alex-Acorn <77464408+Alex-Acorn@users.noreply.github.com>
2021-03-21 18:22:17 -0400

    Implemented More Sounds-Spike Kill, Ambient level sounds, Running Water sounds

---
+++ Group3_project/Assets/PlayAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
