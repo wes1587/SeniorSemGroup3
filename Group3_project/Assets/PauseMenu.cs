25cd869a3a2f1f1cae0e358634a660e51e6df7ef
Bruna Sousa <brudesousa26@gmail.com>
2021-03-22 15:06:41 -0400

    updates
    pause menu comes up when "esc" is pressed

--- Group3_project/Assets/PauseMenu.cs
+++ Group3_project/Assets/PauseMenu.cs
@@ -11,7 +11,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
 
             if (IsPaused)
