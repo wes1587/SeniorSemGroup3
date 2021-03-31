51abf614d97871bba2b361b6f86cabe7177a0072
Bruna Sousa <brudesousa26@gmail.com>
2021-03-30 21:28:32 -0400

    HealthBar added
    health bar added and integrated with character, still not finished

---
+++ Group3_project/Assets/HealthBarScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // allow us to create a variable to store slider
+
+public class HealthBarScript : MonoBehaviour
+
+{
+    public Slider slider;  //reference to slider
+    public Gradient gradient;
+    public Image fill;
+
+
+    public void SetHealth(int health)
+
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+        
+    }
+
+    public void SetMaxHealth(int health)
+
+    {
+        slider.maxValue = health; //
+        slider.value = health; // because slider needs to start at max value (full life)
+        fill.color=gradient.Evaluate(1f); //so slider starts green
+    }
+
+    
+
+}
