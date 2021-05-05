74682f788235222eee3e2a138ff10b9e82ffe775
Alex-Acorn <77464408+Alex-Acorn@users.noreply.github.com>
2021-05-04 20:39:52 -0400

    More Audio fixes

--- Group3_project/Assets/Scripts/GateTrigger.cs
+++ Group3_project/Assets/Scripts/GateTrigger.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GateTrigger : MonoBehaviour
 {
+    
     [SerializeField]
     GameObject gate;
 
     bool isOpen = false;
+
+    void start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+}
     void OnTriggerEnter(Collider col)
     {
         if (!isOpen)
         {
             isOpen = true;
+            GetComponent<AudioSource>().Play();
             gate.transform.position += new Vector3(0, -12, 0);
         }
 
@@ -22,6 +29,7 @@ public class GateTrigger : MonoBehaviour
         if (isOpen)
         {
             isOpen = false;
+            GetComponent<AudioSource>().Play();
             gate.transform.position += new Vector3(0, 12, 0);
         }
     }
